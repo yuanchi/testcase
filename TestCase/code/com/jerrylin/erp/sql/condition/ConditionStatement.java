fc7cc659e5198a7790613652ef88c6f06e85e2a2
yuanchi <eeit54@gmail.com>
2016-03-17 16:03:24 +0800

    add ConditionStatement representing add condition without input condition value

---
+++ TestCase/code/com/jerrylin/erp/sql/condition/ConditionStatement.java
@@ -0,0 +1,35 @@
+package com.jerrylin.erp.sql.condition;
+
+import com.jerrylin.erp.sql.ISqlNode;
+
+/**
+ * representing single condition statement without extra input value
+ * ex: p.name IS NOT NULL
+ * @author JerryLin
+ *
+ */
+public class ConditionStatement extends SqlCondition {
+	private static final long serialVersionUID = -6344870109542752135L;
+	
+	private String expression;
+	
+	public String getExpression() {
+		return expression;
+	}
+	public void setExpression(String expression) {
+		this.expression = expression;
+	}
+
+	@Override
+	public String genSql() {
+		return getJunction().getSymbol() + expression;
+	}
+	@Override
+	public ISqlNode singleCopy() {
+		ConditionStatement s = new ConditionStatement();
+		s.id(getId());
+		s.setExpression(expression);
+		return s;
+	}
+
+}
