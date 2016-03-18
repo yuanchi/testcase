c08aa0ed5c66a9cff1797c343919c6f7ab070d46
yuanchi <eeit54@gmail.com>
2016-03-18 17:55:42 +0800

    1.method filterCopy rename to filterOutCopy 2.add constant: INCLUDED and EXCLUDED 3.add method: adjustStrConditionNode 4.add testCase: testAdjustStrConditionNode

--- TestCase/code/com/jerrylin/erp/sql/execute/SqlNodeManager.java
+++ TestCase/code/com/jerrylin/erp/sql/execute/SqlNodeManager.java
@@ -1,6 +1,11 @@
 package com.jerrylin.erp.sql.execute;
 
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.jerrylin.erp.function.PredicateThrowable;
 import com.jerrylin.erp.sql.ISqlNode;
@@ -8,6 +13,8 @@ import com.jerrylin.erp.sql.ISqlRoot;
 import com.jerrylin.erp.sql.SqlRoot;
 import com.jerrylin.erp.sql.Where;
 import com.jerrylin.erp.sql.condition.SimpleCondition;
+import com.jerrylin.erp.sql.condition.StrCondition;
+import com.jerrylin.erp.sql.condition.StrCondition.MatchMode;
 
 /**
  * config and reuse SqlRoot
@@ -15,6 +22,9 @@ import com.jerrylin.erp.sql.condition.SimpleCondition;
  *
  */
 public class SqlNodeManager {
+	private static final boolean EXCLUDED = true;
+	private static final boolean INCLUDED = false;
+	
 	private SqlRoot root;
 	public SqlNodeManager(){
 		root = SqlRoot.getInstance();
@@ -48,11 +58,11 @@ public class SqlNodeManager {
 	 * @param filter
 	 * @return
 	 */
-	public ISqlNode filterCopy(PredicateThrowable<ISqlNode> filter){
-		return filterCopy(root, filter);
+	public ISqlNode filterOutCopy(PredicateThrowable<ISqlNode> filter){
+		return filterOutCopy(root, filter);
 	}
-	private ISqlNode filterCopy(ISqlNode src, PredicateThrowable<ISqlNode> filter){
-		if(filter.test(src)){
+	private ISqlNode filterOutCopy(ISqlNode src, PredicateThrowable<ISqlNode> filterout){
+		if(filterout.test(src)){
 			return null;
 		}
 		ISqlNode copyNode = src.singleCopy();
@@ -60,7 +70,7 @@ public class SqlNodeManager {
 		if(nodes.size() != 0){
 			for(int i = 0; i < nodes.size(); i++){
 				ISqlNode child = nodes.get(i);
-				ISqlNode childCopy = filterCopy(child, filter);
+				ISqlNode childCopy = filterOutCopy(child, filterout);
 				if(childCopy != null){
 					copyNode.addChild(childCopy);
 				}
@@ -68,6 +78,44 @@ public class SqlNodeManager {
 		}
 		return copyNode;
 	}
+	/**
+	 * according to config, adjust string condition node expression
+	 * @param node
+	 */
+	private void adjustStrConditionNode(ISqlNode node){
+		if(node instanceof StrCondition){
+			StrCondition cond = (StrCondition)node;
+			MatchMode mode = cond.getMatchMode();
+			cond.value(mode.transformer((String)cond.getValue()));
+						
+			if(cond.isCaseInsensitive()){
+				cond.propertyName("lower(" + cond.getPropertyName() + ")");
+				cond.value(((String)cond.getValue()).toLowerCase());
+			}
+		}
+		List<ISqlNode> nodes = node.getChildren();
+		nodes.forEach(child->{
+			adjustStrConditionNode(child);
+		});
+		
+	}
+	
+	private Map<String, Object> getParams(ISqlNode node){
+		Map<String, Object> params = new LinkedHashMap<>();
+		getParams(node, params);
+		return params;
+	}
+	
+	private void getParams(ISqlNode node, Map<String, Object> params){
+		if(node instanceof SimpleCondition){
+			SimpleCondition sc = (SimpleCondition)node;
+			params.put(sc.getId(), sc.getValue());
+		}
+		node.getChildren().forEach(child->{
+			getParams(child, params);
+		});
+	}
+	
 	private static void testFindConditionById(){
 		SqlNodeManager exe = new SqlNodeManager();
 		ISqlRoot root = exe.getRoot()
@@ -130,19 +178,90 @@ public class SqlNodeManager {
 				.getRoot()
 			;
 		
-		ISqlNode copy = exe.filterCopy(n->{return false;});
+		ISqlNode copy = exe.filterOutCopy(n->{return INCLUDED;});
 		System.out.println(copy.genSql());
 		
-		ISqlNode copy2 = exe.filterCopy(n->{
+		ISqlNode copy2 = exe.filterOutCopy(n->{
 			if(n instanceof Where){
-				return true;
+				return EXCLUDED;
 			}
-			return false;
+			return INCLUDED;
 		});
 		System.out.println(copy2.genSql());
+		System.out.println(exe.getRoot() == copy2);
+	}
+	
+	private static void testAdjustStrConditionNode(){
+		SqlNodeManager exe = new SqlNodeManager();
+		ISqlRoot root = exe.getRoot()
+			.select()
+				.target("p1.id", "pId")
+				.target("p1.name", "pName")
+				.target("p2.mobile", "pMobile")
+				.target("p2.tel", "pTel")
+				.getRoot()
+			.from()
+				.target("com.jerrylin.erp.model.Member", "p1")
+				.target("com.jerrylin.erp.model.Member", "p2")
+				.getRoot()
+			.joinAlias("LEFT JOIN p1.orders", "p1Orders")
+			.where()
+				.andConds()
+					.andStrCondition("p1.name LIKE :pName", MatchMode.START, "John")
+					.andSimpleCond("p1.age = :pAge", Integer.class)
+					.getWhere()
+				.orConds()
+					.andStrCaseInsensitive("p2.address LIKE :pAddress", MatchMode.ANYWHERE, "AsxhyyOOPP")
+					.andSimpleCond("p2.gender = :pGender", Integer.class)
+					.getRoot()
+			.orderBy()
+				.asc("p1.id")
+				.desc("p2.name")
+				.getRoot()
+			;
+		
+		ISqlNode copy = exe.filterOutCopy(n->{
+			if(!(n instanceof SimpleCondition)){
+				return INCLUDED;
+			}
+			SimpleCondition sc = (SimpleCondition)n;
+			Object val = sc.getValue();
+			if(val == null){
+				return EXCLUDED;
+			}
+			if(val instanceof String){
+				String strVal = (String)val;
+				if(StringUtils.isBlank(strVal)){
+					return EXCLUDED;
+				}
+			}
+			if(val instanceof Collection){
+				Collection<?> collect = (Collection<?>)val;
+				if(collect.size() == 0){
+					return EXCLUDED;
+				}
+			}
+			if(val instanceof Object[]){
+				Object[] objArray = (Object[])val;
+				if(objArray.length == 0){
+					return EXCLUDED;
+				}
+			}
+			return INCLUDED;});
+		exe.adjustStrConditionNode(copy);
+		System.out.println(copy.genSql());
+		Map<String, Object> params = exe.getParams(copy);
+		System.out.println("");
+		System.out.println("params key value: ");
+		params.forEach((k,v)->{
+			System.out.println(k + ":" + v);
+		});
+		
+		
+		
 	}
 	
 	public static void main(String[]args){
-		testFilterCopy();
+		testAdjustStrConditionNode();
 	}
 }
