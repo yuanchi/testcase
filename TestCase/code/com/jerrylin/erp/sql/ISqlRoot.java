cd6a7d4f4836bf9752d0a247399d97ea1c19b3a4
yuanchi <eeit54@gmail.com>
2016-03-20 14:17:25 +0800

    add excludeCopy method to ISqlRoot

--- TestCase/code/com/jerrylin/erp/sql/ISqlRoot.java
+++ TestCase/code/com/jerrylin/erp/sql/ISqlRoot.java
@@ -1,10 +1,16 @@
 package com.jerrylin.erp.sql;
 
+import java.util.function.Predicate;
+
 public interface ISqlRoot extends ISqlNode {
+	public static final boolean EXCLUDED = Boolean.TRUE;
+	public static final boolean INCLUDED = Boolean.FALSE;
+	
 	public Select select();
 	public From from();
 	public Where where();
 	public ISqlRoot joinAlias(String expression, String alias);
 	public ISqlRoot joinOn(String expression, String on);
 	public OrderBy orderBy();
+	public ISqlNode excludeCopy(Predicate<ISqlNode> exclude);
 }
