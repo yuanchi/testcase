d7e609b65efb66823574595197da807893de6360
yuanchi <eeit54@gmail.com>
2016-03-22 17:27:25 +0800

    ConditionalQuery.java: 1. add method excludeNode and transformNode to enhance flexibility 2. all main output methods change to base on copyRoot preprocessed

--- TestCase/code/com/jerrylin/erp/query/ConditionalQuery.java
+++ TestCase/code/com/jerrylin/erp/query/ConditionalQuery.java
@@ -1,18 +1,13 @@
 package com.jerrylin.erp.query;
 
 import java.io.Serializable;
+import java.sql.Date;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
-
-
-
-
-
-
-
+import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,16 +16,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-
-
-
-
-
-
 import com.jerrylin.erp.component.SessionFactoryWrapper;
 import com.jerrylin.erp.model.Member;
+import com.jerrylin.erp.service.TimeService;
 import com.jerrylin.erp.sql.From;
 import com.jerrylin.erp.sql.ISqlNode;
+import com.jerrylin.erp.sql.ISqlRoot;
 import com.jerrylin.erp.sql.OrderBy;
 import com.jerrylin.erp.sql.Select;
 import com.jerrylin.erp.sql.SqlRoot;
@@ -40,16 +31,27 @@ import com.jerrylin.erp.test.BaseTest;
 
 @Service
 @Scope("prototype")
+/**
+ * basic implementation of querying by condition, including common and paging query
+ * required: configure sqlRoot
+ * required: confirm (and configure if needed) primary key name; default is 'id', representing mapping entity has a field name 'id' as primary key 
+ * @author JerryLin
+ *
+ * @param <T>
+ */
 public class ConditionalQuery<T> implements Serializable{
 	private static final long serialVersionUID = 5747837071652296027L;
 	private static final String ID_FIELD = "id";
 	
 	private Logger logger = Logger.getLogger(ConditionalQuery.class.getName());
+	private String idFieldName = ID_FIELD;
 	
 	@Autowired
 	private SessionFactoryWrapper sfw;
 	@Autowired
 	private SqlRoot sqlRoot;
+	@Autowired
+	private TimeService timeService;
 	
 	private int currentPage = 1;
 	private int countPerPage = 10;
@@ -70,40 +72,59 @@ public class ConditionalQuery<T> implements Serializable{
 	public void setCountPerPage(int countPerPage) {
 		this.countPerPage = countPerPage;
 	}
-
 	public PageNavigator getPageNavigator() {
 		return pageNavigator;
 	}
-
+	public String getIdFieldName(){
+		return idFieldName;
+	}
+	public void setIdFieldName(String idFieldName){
+		this.idFieldName = idFieldName;
+	}
+	
 	public List<T> executeQueryPageable(){
 		List<T> results = sfw.executeSession(s->{
 			return executeQueryPageable(s);
 		});
 		return results;
 	}
+	/**
+	 * paging query
+	 * attention: NOT use 'JOIN fetch' syntax here
+	 * @param s
+	 * @return
+	 */
 	public List<T> executeQueryPageable(Session s){
 		String alias = findFirstSqlTargetAlias();
-		String identifier = alias + "." + ID_FIELD;
-		String select = getSelectSql();
-		String where = getWhereSql();
+		String id = getIdFieldName();
+		String identifier = alias + "." + id;
 		addOrderByIdIfAnyNotExisted();
-		String rootSql = sqlRoot.genSql();
+		
+		// retrieving copy root, not originally
+		SqlRoot copyRoot = copyRootPrepared(); 
+		String rootSql = copyRoot.genSql();
+		String select = getSelectSql(copyRoot);
+		String where = getWhereSql(copyRoot);
+		
+		if(rootSql.toLowerCase().contains("join fetch")){
+			throw new UnsupportedOperationException("ConditionalQuery.executeQueryPageable NOT SUPPORT join fetch synctax!!");
+		}
 		
 		String selectCount = "SELECT COUNT(DISTINCT " + identifier + ")";
 		String selectCountHql = rootSql.replace(select, selectCount);
-		logger.log(Level.WARNING, "selectCountHql: " + selectCountHql + "\n");
+		logger.log(Level.INFO, "selectCountHql: " + selectCountHql + "\n");
 		
 		String selectId = "SELECT DISTINCT " + identifier;
 		String selectIdHql = rootSql.replace(select, selectId);
-		logger.log(Level.WARNING, "selectIdHql: " + selectIdHql + "\n");
+		logger.log(Level.INFO, "selectIdHql: " + selectIdHql + "\n");
 		
 		String selectAlias = "SELECT DISTINCT " + alias;
 		String whereInIds = "WHERE " + identifier + " IN (:ids)";
 		String whereInIdsHql = rootSql.replace(select, selectAlias)
 									.replace(where, whereInIds);
-		logger.log(Level.WARNING, "whereInIdsHql: " + whereInIdsHql + "\n");
+		logger.log(Level.INFO, "whereInIdsHql: " + whereInIdsHql + "\n");
 		
-		Map<String, Object> params = sqlRoot.getCondIdValuePairs();
+		Map<String, Object> params = copyRoot.getCondIdValuePairs();
 		
 		List<T> results = Collections.emptyList();
 		int totalCount = 0;
@@ -128,6 +149,50 @@ public class ConditionalQuery<T> implements Serializable{
 		return results;
 	}
 
+	public List<T> executeQueryList(Session s){
+		SqlRoot copyRoot = copyRootPrepared();
+		String queryHql = copyRoot.genSql();
+		Map<String, Object> params = copyRoot.getCondIdValuePairs();
+		List<T> results = s.createQuery(queryHql).setProperties(params).list();
+		return results;
+	}
+	
+	public List<T> executeQueryList(){
+		List<T> r = sfw.executeSession(s->{
+			List<T> results = executeQueryList(s);
+			return results;
+		});
+		return r;
+	}
+	/**
+	 * this is the BASIC starting point of all OUTPUT methods on ConditionalQuery.
+	 * first exclude nodes not required and copy,
+	 * secondly transform nodes meeting the criteria and copy,
+	 * lastly get copy root node
+	 * @return
+	 */
+	SqlRoot copyRootPrepared(){
+		ISqlRoot copy1 = sqlRoot.excludeCopy(excludeNode());
+		ISqlRoot copy2 = copy1.transformCopy(transformNode());
+		return (SqlRoot)copy2;
+	}
+	/**
+	 * provide exclude node predicate
+	 * default use SqlRoot built-in excludeNode
+	 * @return
+	 */
+	Predicate<ISqlNode> excludeNode(){
+		return sqlRoot.excludeNode();
+	}
+	/**
+	 * provide transform node consumer
+	 * default use SqlRoot built-in transformNode
+	 * @return
+	 */
+	Consumer<ISqlNode> transformNode(){
+		return sqlRoot.transformNode();
+	}
+	
 	private String findFirstSqlTargetAlias(){
 		ISqlNode target = sqlRoot
 			.find(n-> (n instanceof SqlTarget && n.getParent() instanceof From))
@@ -138,8 +203,8 @@ public class ConditionalQuery<T> implements Serializable{
 		return alias;
 	}
 	
-	private String getSelectSql(){
-		ISqlNode target = sqlRoot
+	private String getSelectSql(SqlRoot root){
+		ISqlNode target = root
 			.find(n-> (n instanceof Select))
 			.getFounds()
 			.get(0);
@@ -148,8 +213,8 @@ public class ConditionalQuery<T> implements Serializable{
 		return sql;
 	}
 	
-	private String getWhereSql(){
-		ISqlNode target = sqlRoot
+	private String getWhereSql(SqlRoot root){
+		ISqlNode target = root
 			.find(n-> (n instanceof Where))
 			.getFounds()
 			.get(0);
@@ -170,7 +235,8 @@ public class ConditionalQuery<T> implements Serializable{
 		}
 		if(node.getChildren().isEmpty()){
 			String alias = findFirstSqlTargetAlias();
-			String identifier = alias + "." + ID_FIELD;
+			String id = getIdFieldName();
+			String identifier = alias + "." + id;
 			node.desc(identifier);
 		}
 	}
@@ -189,15 +255,27 @@ public class ConditionalQuery<T> implements Serializable{
 					.target("p", "member").getRoot()
 				.from()
 					.target(Member.class.getName(), "p").getRoot()
+				.joinAlias("LEFT JOIN p.vipDiscountDetails", "vipDetails")
 				.where()
 					.andConds()
-						.andSimpleCond("p.idNo LIKE :pIdNo", String.class, "P%").getRoot()
+						.andSimpleCond("p.idNo LIKE :pIdNo", String.class, "P%")
+						.andSimpleCond("vipDetails.effectiveEnd > :vEffectiveEnd", Date.class, c.timeService.atStartOfToday())
+						.andSimpleCond("p.name LIKE :pName", String.class).getRoot()
 				.orderBy()
 					.desc("p.idNo");
+			
+			List<Object> r = c.executeQueryList();
+			r.forEach(o->{
+				Member m = (Member)o;
+				System.out.println(m.getId());
+			});
+			
 			List<Object> results = c.executeQueryPageable();
 			results.forEach(o->{
 				Member m = (Member)o;
-				System.out.println(m.getName());
+				System.out.println(m.getId() + "|" + m.getName());
+//				List<VipDiscountDetail> details = m.getVipDiscountDetails();
+//				System.out.println("details count: " + details.size());
 			});
 		});
 	}
