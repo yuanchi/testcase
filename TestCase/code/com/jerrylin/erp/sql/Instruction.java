4821ea8dfefbfc4a51e5d7ef968aeb9a9a130223
yuanchi <eeit54@gmail.com>
2016-03-22 17:12:45 +0800

    add Instruction.java to define syntax used by SimpleCondition

---
+++ TestCase/code/com/jerrylin/erp/sql/Instruction.java
@@ -0,0 +1,9 @@
+package com.jerrylin.erp.sql;
+
+public class Instruction {
+	public static final String START_LIKE = "START_WITH_LIKE_PATTERN";
+	public static final String END_LIKE = "END_WITH_LIKE_PATTERN";
+	public static final String CONTAIN_LIKE = "CONTAIN_BETWEEN_LIKE_PATTERN";
+	public static final String LOWERCASE = "LOWERCASE";
+	public static final String UPPERCASE = "UPPERCASE";
+}
