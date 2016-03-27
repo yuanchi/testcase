195cc02ee0075305f98f9ddd01b41d105300f3eb
yuanchi <eeit54@gmail.com>
2016-03-27 15:28:09 +0800

    MemberController.java add method: queryConditional

--- TestCase/code/com/jerrylin/erp/controller/MemberController.java
+++ TestCase/code/com/jerrylin/erp/controller/MemberController.java
@@ -5,11 +5,13 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jerrylin.erp.component.ConditionConfig;
+import com.jerrylin.erp.jackson.mixin.MemberIgnoreDetail;
 import com.jerrylin.erp.model.Member;
 import com.jerrylin.erp.service.QueryBaseService;
 import com.jerrylin.erp.util.JsonParseUtil;
@@ -41,7 +43,25 @@ public class MemberController {
 			headers="Accept=*/*")
 	public @ResponseBody String queryAll(){
 		ConditionConfig<Member> cc = queryBaseService.genCondtitionsAfterExecuteQueryPageable();
-		String json = JsonParseUtil.parseToJson(cc);
+		String json = conditionConfigToJsonStr(cc);
+		return json;
+	}
+	
+	/**
+	 * 分頁條件查詢
+	 */
+	@RequestMapping(value="/queryConditional",
+			method=RequestMethod.POST,
+			produces={"application/xml", "application/json"},
+			headers="Accept=*/*")
+	public @ResponseBody String queryConditional(@RequestBody ConditionConfig<Member> conditionConfig){
+		ConditionConfig<Member> cc = queryBaseService.executeQueryPageable(conditionConfig);
+		String result = conditionConfigToJsonStr(cc);
+		return result;
+	}
+	
+	private String conditionConfigToJsonStr(ConditionConfig<Member> cc){
+		String json = JsonParseUtil.parseToJson(cc, Member.class, MemberIgnoreDetail.class);
 		return json;
 	}
 }
