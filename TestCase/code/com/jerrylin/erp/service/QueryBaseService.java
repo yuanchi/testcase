b7f30137ea26dd09a4048d47b3bb5d6f7ce75353
yuanchi <eeit54@gmail.com>
2016-04-03 17:30:53 +0800

    QueryBaseService.java add logic to process kendo ui grid complex filter

--- TestCase/code/com/jerrylin/erp/service/QueryBaseService.java
+++ TestCase/code/com/jerrylin/erp/service/QueryBaseService.java
@@ -57,6 +57,10 @@ public class QueryBaseService<T, R> implements Serializable{
 	@Resource(name="classFieldType")
 	private Map<Class<?>, Map<String, Class<?>>> classFieldType;
 	
+	private int filterCount;
+	private String alias;
+	private SqlTarget target;
+	
 	public ConditionConfig<T> copyToConditionConfig(){
 		ConditionConfig<T> cc = new ConditionConfig<T>();
 		Map<String, Object> conds = cc.getConds();
@@ -79,14 +83,14 @@ public class QueryBaseService<T, R> implements Serializable{
 	public void copyFromConditionConfig(ConditionConfig<T> conds){
 		SqlRoot root = getSqlRootImpl();
 		Map<String, Object> all = conds.getConds();
-		SqlTarget target = q.findFirstSqlTarget();
+		SqlTarget target = getFirstSqlTarget();
 		// predefined conditions populate value
 		all.keySet().stream().filter(k->k.startsWith(SIMPLE_CONDITION_PREFIX)).forEach(k->{
 			String id = k.replace(SIMPLE_CONDITION_PREFIX, "");
 			List<ISqlNode> founds = root.findNodeById(id).getFounds();
 			if(founds.size() == 1){
 				SimpleCondition s = (SimpleCondition)founds.get(0);
-				addValToSimpleCondition(s, all.get(k), target);
+				addValToSimpleCondition(s, all.get(k));
 			}
 		});
 		
@@ -117,7 +121,7 @@ public class QueryBaseService<T, R> implements Serializable{
 			}
 			orderBy.getChildren().clear();
 			
-			String alias = q.findFirstSqlTargetAlias();
+			String alias = getAlias();
 			for(int i = 0; i < orderTypes.size(); i++){
 				Map<String, String> orderType = orderTypes.get(i); // Kendo UI Grid排序回傳的資料結構 
 				String field = orderType.get("field");
@@ -142,104 +146,239 @@ public class QueryBaseService<T, R> implements Serializable{
 			where = root.where();
 		}
 		
-		if(KENDO_UI_FILTER_LOGIC_AND.equals(logic)){
-			CollectConds conds = null;
-			List<ISqlNode> children =where.getChildren();
-			if(!children.isEmpty()){
-				ISqlNode lastChild = children.get(children.size()-1);
-				CollectConds last = (CollectConds)lastChild;
-				if(last.getJunction() == Junction.AND){
-					conds = last;
-				}
+		CollectConds conds = null;
+		List<ISqlNode> children = where.getChildren();
+		if(!children.isEmpty()){
+			ISqlNode lastChild = children.get(children.size()-1);
+			CollectConds last = (CollectConds)lastChild;
+			if(last.getJunction() == Junction.AND){
+				conds = last;
 			}
-			
-			if(conds == null){
-				conds = where.andConds();
+		}
+		
+		if(conds == null){
+			conds = where.andConds();
+		}
+		
+		filterCount = 0;
+		addFilterCondtions(filters, conds, logic);
+		
+//		if(KENDO_UI_FILTER_LOGIC_AND.equals(logic)){
+//			CollectConds conds = null;
+//			List<ISqlNode> children =where.getChildren();
+//			if(!children.isEmpty()){
+//				ISqlNode lastChild = children.get(children.size()-1);
+//				CollectConds last = (CollectConds)lastChild;
+//				if(last.getJunction() == Junction.AND){
+//					conds = last;
+//				}
+//			}
+//			
+//			if(conds == null){
+//				conds = where.andConds();
+//			}
+//			conds.enableGroupMark(GROUP_AS_KENDO_UI_FILTER);
+//			
+//			ListIterator<Map<String, Object>> iterator = filters.listIterator();
+//			String alias = q.findFirstSqlTargetAlias();
+//			int count = 0;
+//			SqlTarget target = q.findFirstSqlTarget();
+//			while(iterator.hasNext()){
+//				Map<String, Object> f = iterator.next();
+//				String operator = (String)f.get("operator");
+//				String field = (String)f.get("field");
+//				Object value = f.get("value");
+//				
+//				count++;
+//				
+//				String expression = alias + "." + field + " ";
+//				String nameParam = " :" + alias + firstLetterToUpperCase(field) + "_FILTER_" + count;
+//				MatchMode matchMode = null;
+//				Object convertedVal = convertValueByType(field, value, target);
+//				switch(operator){
+//					case "isnull":
+//						expression += "IS NULL";
+//						conds.andStatement(expression);
+//						break;
+//					case "isnotnull":
+//						expression += "IS NOT NULL";
+//						conds.andStatement(expression);
+//						break;
+//					case "isempty":
+//						expression += "IS EMPTY";
+//						conds.andStatement(expression);
+//						break;
+//					case "isnotempty":
+//						expression += "IS NOT EMPTY";
+//						conds.andStatement(expression);
+//						break;
+//						
+//					case "eq":
+//						expression += ("=" + nameParam);
+//						conds.andSimpleCond(expression, value.getClass(), convertedVal);
+//						break;						
+//					case "neq":
+//						expression += ("!=" + nameParam);
+//						conds.andSimpleCond(expression, value.getClass(), convertedVal);
+//						break;						
+//					case "gte":
+//						expression += (">=" + nameParam);
+//						conds.andSimpleCond(expression, value.getClass(), convertedVal);
+//						break;						
+//					case "gt":
+//						expression += (">" + nameParam);
+//						conds.andSimpleCond(expression, value.getClass(), convertedVal);
+//						break;						
+//					case "lte":
+//						expression += ("<=" + nameParam);
+//						conds.andSimpleCond(expression, value.getClass(), convertedVal);
+//						break;						
+//					case "lt":
+//						expression += ("<" + nameParam);
+//						conds.andSimpleCond(expression, value.getClass(), convertedVal);
+//						break;
+//						
+//					case "startswith":
+//						matchMode = MatchMode.START;
+//						expression += ("LIKE" + nameParam);
+//						conds.andStrCondition(expression, matchMode, (String)value);						
+//						break;						
+//					case "endswith":
+//						matchMode = MatchMode.END;
+//						expression += ("LIKE" + nameParam);
+//						conds.andStrCondition(expression, matchMode, (String)value);						
+//						break;						
+//					case "contains":
+//						matchMode = MatchMode.ANYWHERE;
+//						expression += ("LIKE" + nameParam);
+//						conds.andStrCondition(expression, matchMode, (String)value);						
+//						break;
+//					case "doesnotcontain":
+//						matchMode = MatchMode.ANYWHERE;
+//						expression += ("NOT LIKE" + nameParam);
+//						conds.andStrCondition(expression, matchMode, (String)value);
+//						break;
+//				}
+//			}
+//			conds.disableGroupMark();
+//		}
+	}
+	
+	private void addFilterCondtions(List<Map<String, Object>> filters, CollectConds parent, String ParentLogic){
+		parent.enableGroupMark(GROUP_AS_KENDO_UI_FILTER);
+		for(int i = 0; i < filters.size(); i++){
+			Map<String, Object> filter = filters.get(i);
+			String logic = (String)filter.get("logic");
+			if(StringUtils.isNotBlank(logic)){
+				List<Map<String, Object>> f= (List<Map<String, Object>>)filter.get("filters");
+				addFilterCondtions(f, parent.andCollectConds(), logic);
+			}else{
+				addFilterCondition(filter, parent, ParentLogic);
 			}
-			conds.enableGroupMark(GROUP_AS_KENDO_UI_FILTER);
-			
-			ListIterator<Map<String, Object>> iterator = filters.listIterator();
-			String alias = q.findFirstSqlTargetAlias();
-			int count = 0;
-			SqlTarget target = q.findFirstSqlTarget();
-			while(iterator.hasNext()){
-				Map<String, Object> f = iterator.next();
-				String operator = (String)f.get("operator");
-				String field = (String)f.get("field");
-				Object value = f.get("value");
+		}
+		parent.disableGroupMark();
+	}
+	
+	private void addFilterCondition(Map<String, Object> f, CollectConds conds, String logic){
+		String operator = (String)f.get("operator");
+		String field = (String)f.get("field");
+		Object value = f.get("value");
+		
+		filterCount++;
+		
+		String alias = getAlias();
+		String expression = alias + "." + field + " ";
+		String nameParam = " :" + alias + firstLetterToUpperCase(field) + "_FILTER_" + filterCount;
+		MatchMode matchMode = null;
+		Object convertedVal = convertValueByType(field, value);
+		switch(operator){
+			case "isnull":
+				expression += "IS NULL";
+				addStatement(conds, expression, logic);
+				break;
+			case "isnotnull":
+				expression += "IS NOT NULL";
+				addStatement(conds, expression, logic);
+				break;
+			case "isempty":
+				expression += "IS EMPTY";
+				addStatement(conds, expression, logic);
+				break;
+			case "isnotempty":
+				expression += "IS NOT EMPTY";
+				addStatement(conds, expression, logic);
+				break;
 				
-				count++;
+			case "eq":
+				expression += ("=" + nameParam);
+				addSimpleCond(conds, expression, logic, value, convertedVal);
+				break;						
+			case "neq":
+				expression += ("!=" + nameParam);
+				addSimpleCond(conds, expression, logic, value, convertedVal);
+				break;						
+			case "gte":
+				expression += (">=" + nameParam);
+				addSimpleCond(conds, expression, logic, value, convertedVal);
+				break;						
+			case "gt":
+				expression += (">" + nameParam);
+				addSimpleCond(conds, expression, logic, value, convertedVal);
+				break;						
+			case "lte":
+				expression += ("<=" + nameParam);
+				addSimpleCond(conds, expression, logic, value, convertedVal);
+				break;						
+			case "lt":
+				expression += ("<" + nameParam);
+				addSimpleCond(conds, expression, logic, value, convertedVal);
+				break;
 				
-				String expression = alias + "." + field + " ";
-				String nameParam = " :" + alias + firstLetterToUpperCase(field) + "_FILTER_" + count;
-				MatchMode matchMode = null;
-				Object convertedVal = convertValueByType(field, value, target);
-				switch(operator){
-					case "isnull":
-						expression += "IS NULL";
-						conds.andStatement(expression);
-						break;
-					case "isnotnull":
-						expression += "IS NOT NULL";
-						conds.andStatement(expression);
-						break;
-					case "isempty":
-						expression += "IS EMPTY";
-						conds.andStatement(expression);
-						break;
-					case "isnotempty":
-						expression += "IS NOT EMPTY";
-						conds.andStatement(expression);
-						break;
-						
-					case "eq":
-						expression += ("=" + nameParam);
-						conds.andSimpleCond(expression, value.getClass(), convertedVal);
-						break;						
-					case "neq":
-						expression += ("!=" + nameParam);
-						conds.andSimpleCond(expression, value.getClass(), convertedVal);
-						break;						
-					case "gte":
-						expression += (">=" + nameParam);
-						conds.andSimpleCond(expression, value.getClass(), convertedVal);
-						break;						
-					case "gt":
-						expression += (">" + nameParam);
-						conds.andSimpleCond(expression, value.getClass(), convertedVal);
-						break;						
-					case "lte":
-						expression += ("<=" + nameParam);
-						conds.andSimpleCond(expression, value.getClass(), convertedVal);
-						break;						
-					case "lt":
-						expression += ("<" + nameParam);
-						conds.andSimpleCond(expression, value.getClass(), convertedVal);
-						break;
-						
-					case "startswith":
-						matchMode = MatchMode.START;
-						expression += ("LIKE" + nameParam);
-						conds.andStrCondition(expression, matchMode, (String)value);						
-						break;						
-					case "endswith":
-						matchMode = MatchMode.END;
-						expression += ("LIKE" + nameParam);
-						conds.andStrCondition(expression, matchMode, (String)value);						
-						break;						
-					case "contains":
-						matchMode = MatchMode.ANYWHERE;
-						expression += ("LIKE" + nameParam);
-						conds.andStrCondition(expression, matchMode, (String)value);						
-						break;
-					case "doesnotcontain":
-						matchMode = MatchMode.ANYWHERE;
-						expression += ("NOT LIKE" + nameParam);
-						conds.andStrCondition(expression, matchMode, (String)value);
-						break;
-				}
-			}
-			conds.disableGroupMark();
+			case "startswith":
+				matchMode = MatchMode.START;
+				expression += ("LIKE" + nameParam);
+				addStrCond(conds, expression, value, matchMode, logic);
+				break;						
+			case "endswith":
+				matchMode = MatchMode.END;
+				expression += ("LIKE" + nameParam);
+				addStrCond(conds, expression, value, matchMode, logic);						
+				break;						
+			case "contains":
+				matchMode = MatchMode.ANYWHERE;
+				expression += ("LIKE" + nameParam);
+				addStrCond(conds, expression, value, matchMode, logic);						
+				break;
+			case "doesnotcontain":
+				matchMode = MatchMode.ANYWHERE;
+				expression += ("NOT LIKE" + nameParam);
+				addStrCond(conds, expression, value, matchMode, logic);
+				break;
+		}
+	}
+	
+	private void addStrCond(CollectConds conds, String expression, Object value, MatchMode matchMode, String logic){
+		System.out.println("addStrCond logic: " + logic);
+		if("and".equals(logic)){
+			conds.andStrCondition(expression, matchMode, (String)value);
+		}else{
+			conds.orStrCondition(expression, matchMode, (String)value);
+		}
+	}
+	
+	private void addStatement(CollectConds conds, String expression, String logic){
+		if("and".equals(logic)){
+			conds.andStatement(expression);
+		}else{
+			conds.orStatement(expression);
+		}
+	}
+	
+	private void addSimpleCond(CollectConds conds, String expression, String logic, Object value, Object convertedVal){
+		if("and".equals(logic)){
+			conds.andSimpleCond(expression, value.getClass(), convertedVal);
+		}else{
+			conds.orSimpleCond(expression, value.getClass(), convertedVal);
 		}
 	}
 	
@@ -278,18 +417,18 @@ public class QueryBaseService<T, R> implements Serializable{
 		return (SqlRoot)getSqlRoot();
 	}
 	
-	private void addValToSimpleCondition(SimpleCondition s, Object obj, SqlTarget target){
+	private void addValToSimpleCondition(SimpleCondition s, Object obj){
 		if(obj == null){
 			return;
 		}
 //		Class<?> type = s.getType();
-		String field = s.getPropertyName().replace(target.getAlias() + ".", "");
-		Object casted = convertValueByType(field, obj, target);
+		String field = s.getPropertyName().replace(getFirstSqlTarget().getAlias() + ".", "");
+		Object casted = convertValueByType(field, obj);
 		s.value(casted);
 	}
 	
-	private Object convertValueByType(String field, Object obj, SqlTarget target){
-		Class<?> targetClass = target.getTargetClass();
+	private Object convertValueByType(String field, Object obj){
+		Class<?> targetClass = getFirstSqlTarget().getTargetClass();
 		Map<String, Class<?>> types = classFieldType.get(targetClass);
 		Class<?> type = types.get(field);
 		
@@ -326,6 +465,20 @@ public class QueryBaseService<T, R> implements Serializable{
 			throw new RuntimeException(e);
 		}
 		return casted;
+	}
+	
+	private SqlTarget getFirstSqlTarget(){
+		if(target == null){
+			target = q.findFirstSqlTarget();
+		}
+		return target;
+	}
+	
+	private String getAlias(){
+		if(StringUtils.isBlank(alias)){
+			alias = q.findFirstSqlTargetAlias();
+		}
+		return alias;
 	}
 	
 	private static Integer parseInteger(Object obj){
