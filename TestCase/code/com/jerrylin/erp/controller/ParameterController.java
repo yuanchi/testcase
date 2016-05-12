527fd0e2e61401032e43678979bbc2ea8e90fc6e
yuanchi <eeit54@gmail.com>
2016-05-12 11:35:47 +0800

    ParameterController.java adds member field parameterCategoryQueryService

--- TestCase/code/com/jerrylin/erp/controller/ParameterController.java
+++ TestCase/code/com/jerrylin/erp/controller/ParameterController.java
@@ -1,5 +1,11 @@
 package com.jerrylin.erp.controller;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +17,7 @@ import com.jerrylin.erp.component.ConditionConfig;
 import com.jerrylin.erp.jackson.mixin.ParameterIgnoreLocalNames;
 import com.jerrylin.erp.model.Parameter;
 import com.jerrylin.erp.model.ParameterCategory;
+import com.jerrylin.erp.service.ParameterCategoryQueryService;
 import com.jerrylin.erp.util.JsonParseUtil;
 
 @Controller
@@ -19,6 +26,20 @@ import com.jerrylin.erp.util.JsonParseUtil;
 public class ParameterController extends
 		KendoUiGridController<Parameter, Parameter> {
 	private static final long serialVersionUID = 3627293074582321850L;
+	private static final Map<String, String> filterFieldConverter;
+	static{
+		filterFieldConverter = new LinkedHashMap<>();
+		filterFieldConverter.put("parameterCategory", "parameterCategory.id");
+	}
+	@Autowired
+	private ParameterCategoryQueryService parameterCategoryQueryService;
+	
+	@Override
+	@PostConstruct
+	void init(){
+		super.init();
+		kendoUiGridService.setFilterFieldConverter(filterFieldConverter);
+	}
 	
 	@Override
 	String conditionConfigToJsonStr(Object cc){
@@ -26,12 +47,12 @@ public class ParameterController extends
 		return json;
 	}
 	
-	@RequestMapping(value="/queryParameterCatAutocomplete",
+	@RequestMapping(value="/queryParameterCatDropDownList",
 			method=RequestMethod.POST,
 			produces={"application/xml", "application/json"},
 			headers="Accept=*/*")
-	public @ResponseBody String queryParameterCatAutocomplete(@RequestBody ConditionConfig<ParameterCategory> conditionConfig){
-		String result = kendoUiAutocompleteService.queryParameterCategories(conditionConfig);
+	public @ResponseBody String queryParameterCatDropDownList(@RequestBody ConditionConfig<ParameterCategory> conditionConfig){
+		String result = parameterCategoryQueryService.findTargetList(conditionConfig);
 		return result;
 	}
 }
