ba32395128d1741ed52a575b5dd106993bb101c3
yuanchi <eeit54@gmail.com>
2016-07-15 10:31:10 +0800

    ProductInfoService.java adds methods showProductName and compareTownTalkPrice

--- TestCase/code/com/jerrylin/erp/product/googleapps/service/ProductInfoService.java
+++ TestCase/code/com/jerrylin/erp/product/googleapps/service/ProductInfoService.java
@@ -1,5 +1,8 @@
 package com.jerrylin.erp.product.googleapps.service;
 
+import static com.jerrylin.erp.product.magento.service.ProductService.PRODUCT_NAME;
+import static com.jerrylin.erp.product.magento.service.ProductService.PRODUCT_SKU;
+
 import java.io.InputStream;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -8,6 +11,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 import javax.mail.BodyPart;
@@ -15,8 +19,6 @@ import javax.mail.Message;
 import javax.mail.Multipart;
 import javax.mail.Part;
 import javax.mail.search.SubjectTerm;
-
-
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -30,6 +32,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jerrylin.erp.component.SessionFactoryWrapper;
 import com.jerrylin.erp.model.Product;
+import com.jerrylin.erp.product.magento.service.ProductService;
 import com.jerrylin.erp.service.ReceiveEmailService;
 import com.jerrylin.erp.util.JsonParseUtil;
 
@@ -49,16 +52,34 @@ public class ProductInfoService {
 	private static final String summaryField = "summary";
 	private static final String warnField = "warn";
 	
+	private static final int PRODUCT_NAME_DISPLAY_MODE_CN = 1; // 在符合一般格式要求下，顯示商品名稱中文
+	private static final int PRODUCT_NAME_DISPLAY_MODE_EN = 2; // 在符合一般格式要求下，顯示商品名稱英文
+	private static final int PRODUCT_NAME_DISPLAY_MODE_ALL = 3; // 在符合一般格式要求下，顯示商品名稱全名
+	private static final int PRODUCT_NAME_DISPLAY_MODE_ABNORMAL = 4; // 不符合一般格式要求下，顯示全名
+	
 	@Autowired
 	private ReceiveEmailService receiveEmailService;
 	@Autowired
 	private SessionFactoryWrapper sfw;
+	@Autowired
+	private ProductService productService;
 	
+	/**
+	 * 執行流程如下:<br>
+	 * 手/自動以Google Apps Script的函式sendProductsToEmail將商品資訊寄送到指定信箱<br>
+	 * 自行指定信箱收信<br>
+	 * 與資料庫資料比對<br>
+	 * 若有需要，可另發請求到Magento取得商品資料，最主要的標的是商品(中文)名稱
+	 */
 	public void execute(){
-		Message[] messages = receiveEmailService.receive(new SubjectTerm("ohm-products-"+LocalDate.now().toString()));
+		String today = LocalDate.now().toString();
+		String subject = "ohm-products-"+today;
+		Message[] messages = receiveEmailService.receive(new SubjectTerm(subject)); // 只收當天最新的信件，且格式要正確
 		System.out.println("message count:" + messages.length);
-		if(messages.length == 0){return;}
-		
+		if(messages.length == 0){
+			System.out.println("今日"+today+"沒有找到寄信主旨為["+subject+"]的信件");
+			return;
+		}
 		try{
 			Message message = messages[messages.length-1]; // 取得最後一筆
 			Date d = message.getSentDate();
@@ -98,26 +119,108 @@ public class ProductInfoService {
 			
 			ObjectMapper om = new ObjectMapper();
 			JsonNode root = om.readTree(content);
-			JsonParseUtil.processSingleNode(root, (f,n)->{
+//			JsonParseUtil.processSingleNode(root, (f,n)->{
 //				System.out.println(f);
-			});
-			compare(root);
+//			});
+			JsonNode summaryNode = root.get(summaryField);
+			Iterator<String> summaryFields = summaryNode.fieldNames();
+			List<String> skus = new ArrayList<>();
+			summaryFields.forEachRemaining(skus::add);
+			
+			compare(root, summaryNode, skus);
+			showProductName(skus, PRODUCT_NAME_DISPLAY_MODE_ABNORMAL);
 		}catch(Throwable e){
 			throw new RuntimeException(e);
 		}
 	}
 	@Transactional
-	private void compare(JsonNode root){
+	private void compare(JsonNode root, JsonNode summaryNode, List<String> skus){
 		JsonNode countValNode = root.get(countField);
-		JsonNode summaryNode = root.get(summaryField);
-		JsonNode warnNode = root.get(warnField);
-		
-		Iterator<String> summaryFields = summaryNode.fieldNames();
-		List<String> skus = new ArrayList<>();
-		summaryFields.forEachRemaining(skus::add);
+//		JsonNode warnNode = root.get(warnField);
 		
 		compareCount(countValNode, skus);
 		comparePrice(summaryNode);
+		compareTownTalkPrice(summaryNode);
+	}
+	/**
+	 * 
+	 * @param skus: 這次要比對的商品型號，來自庫存表
+	 * @param displayMode
+	 */
+	private void showProductName(List<String> skus, int displayMode){
+		productService.changeToIntranetUrl(); // 轉向內網
+		String results = productService.listProductsByIds(skus);
+		ObjectMapper om = new ObjectMapper();
+		try{
+			JsonNode root = om.readTree(results);
+			AtomicInteger total = new AtomicInteger(0);
+			AtomicInteger cnCount = new AtomicInteger(0);
+			AtomicInteger enCount = new AtomicInteger(0);
+			AtomicInteger abnormalCount = new AtomicInteger(0);
+			JsonParseUtil.iterate("root", root, (f,n)->{
+				if(n.isContainerNode()
+				&& n.isObject()){
+					Iterator<String> fieldNames = n.fieldNames();
+					String sku = null;
+					while(fieldNames.hasNext()){
+						String fieldName = fieldNames.next();
+						JsonNode valueNode = n.get(fieldName);
+						if(PRODUCT_SKU.equals(fieldName)){
+							sku = valueNode.asText();
+							continue;
+						}
+						if(PRODUCT_NAME.equals(fieldName)){
+							String name = valueNode.asText();
+							if(StringUtils.isNotBlank(name)){
+								if(displayMode == PRODUCT_NAME_DISPLAY_MODE_ALL){
+									System.out.println(sku + ":" + name);
+								}else{
+									String nameCn="";
+									String nameEn="";
+									String[]splits = name.split("\\/");
+									if(splits.length==2){
+										nameEn = splits[0];
+										nameEn = nameEn.trim();
+										nameCn = splits[1];
+										nameCn = nameCn.trim();
+										if(StringUtils.isNotBlank(nameCn)
+										&& displayMode == PRODUCT_NAME_DISPLAY_MODE_CN){
+											cnCount.incrementAndGet();
+											System.out.println(sku + ":" + nameCn);
+										}
+										if(StringUtils.isNotBlank(nameEn)
+										&& displayMode == PRODUCT_NAME_DISPLAY_MODE_EN){
+											enCount.incrementAndGet();
+											System.out.println(sku + ":" + nameEn);
+										}
+									}else if(displayMode == PRODUCT_NAME_DISPLAY_MODE_ABNORMAL){
+										abnormalCount.incrementAndGet();
+										System.out.println(sku + ":" + name);
+									}
+								}
+							}
+							continue;
+						}
+					}
+					if(sku!=null){
+						total.incrementAndGet();
+//						System.out.println("----------------------");
+					}
+				}
+			});
+			System.out.println("商品共"+total.get()+"筆");
+			if(displayMode == PRODUCT_NAME_DISPLAY_MODE_CN){
+				System.out.println("商品名稱有中文共"+cnCount.get()+"筆");
+			}
+			if(displayMode == PRODUCT_NAME_DISPLAY_MODE_EN){
+				System.out.println("商品名稱有英文共"+enCount.get()+"筆");
+			}
+			if(displayMode == PRODUCT_NAME_DISPLAY_MODE_ABNORMAL){
+				System.out.println("商品名稱格式不符慣例共"+abnormalCount.get()+"筆");
+			}
+		}catch(Throwable e){
+			throw new RuntimeException(e);
+		}
 	}
 	private void compareCount(JsonNode countValNode, List<String>remoteSkus){
 		int remoteCount = countValNode.asInt();
@@ -159,6 +262,38 @@ public class ProductInfoService {
 					Session s = sfw.getCurrentSession();
 					String queryPrice = "SELECT p.suggestedRetailPrice FROM " + Product.class.getName() + " p WHERE p.modelId = :modelId";
 					List<Double> dbPrice = s.createQuery(queryPrice).setString("modelId", sku).list();
+					int remotePrice = info.asInt();
+					if(remotePrice > 0
+					&& (dbPrice.isEmpty() || dbPrice.get(0) == 0)){
+						System.out.println(sku + " remotePrice:"+ remotePrice + " dbPrice:"+(dbPrice.isEmpty() ? "empty" : dbPrice.get(0)));
+					}
+				}
+			}
+		});
+	}
+	private void compareTownTalkPrice(JsonNode summaryNode){
+		compareSingle(summaryNode, product->{
+			Iterator<String>infoFields = product.fieldNames();
+			String sku = null;
+			while(infoFields.hasNext()){
+				String infoField = infoFields.next();
+				JsonNode info = product.get(infoField);
+				if(infoField.equals(skuField)){
+					sku = info.asText();
+					if(!sku.startsWith("TT")){
+						break;
+					}
+					continue;
+				}
+				
+				if(infoField.equals(counterPriceField)){ // TownTalk專櫃價比較高，暫且以此對比零售價
+					Session s = sfw.getCurrentSession();
+					String queryPrice = "SELECT p.suggestedRetailPrice FROM " + Product.class.getName() + " p WHERE p.modelId = :modelId";
+					List<Double> dbPrice = s.createQuery(queryPrice).setString("modelId", sku).list();
+					if(info.isTextual()){// TownTalk價格可能是字串，而且無法與數值比較
+						System.out.println(sku + " remotePrice type is string:"+ info.asText() + " dbPrice:"+(dbPrice.isEmpty() ? "empty" : dbPrice.get(0)));
+						continue;
+					}
 					int remotePrice = info.asInt();
 					if(remotePrice > 0
 					&& (dbPrice.isEmpty() || dbPrice.get(0) == 0)){
