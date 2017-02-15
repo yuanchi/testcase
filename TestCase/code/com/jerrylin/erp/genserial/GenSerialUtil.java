285aefad7fd077f597e4f39dc843bc0117cbfae2
yuanchi <eeit54@gmail.com>
2017-02-15 15:36:57 +0800

    add GenSerialUtil.java

---
+++ TestCase/code/com/jerrylin/erp/genserial/GenSerialUtil.java
@@ -0,0 +1,32 @@
+package com.jerrylin.erp.genserial;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.SessionFactory;
+
+public class GenSerialUtil {
+	private static Map<String, SerialGenerator> generators = new HashMap<>();
+	
+	public static synchronized void addGenerator(SessionFactory sessionFactory, String... ids) throws Throwable{
+		for(String id : ids){
+			addGenerator(new DefaultSerialGenerator(id, sessionFactory));
+		}
+	}
+	public static synchronized void addGenerator(SerialGenerator generator) throws Throwable{
+		if(generator.getId() == null){
+			throw new RuntimeException("Generator id can't be null");
+		}
+		if(generators.containsKey(generator.getId())){
+			throw new RuntimeException("Duplicate generator id["+generator.getId()+"]");
+		}
+		generators.put(generator.getId(), generator);
+	}
+	public static synchronized String getNext(String id)throws Throwable{
+		SerialGenerator generator = generators.get(id);
+		if(generator == null){
+			throw new RuntimeException("Serial Generator not found ["+id+"]");
+		}
+		return generator.getNext();
+	}
+}
