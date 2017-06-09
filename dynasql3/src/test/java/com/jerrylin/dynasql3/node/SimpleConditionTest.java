6481e1776deff4850b34ac189577a5fc5adaa414
yuanchi <eeit54@gmail.com>
2017-06-09 16:38:53 +0800

    SimpleConditionTest.java modified

--- dynasql3/src/test/java/com/jerrylin/dynasql3/node/SimpleConditionTest.java
+++ dynasql3/src/test/java/com/jerrylin/dynasql3/node/SimpleConditionTest.java
@@ -1,5 +1,9 @@
 package com.jerrylin.dynasql3.node;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
 
 public class SimpleConditionTest {
@@ -9,5 +13,21 @@ public class SimpleConditionTest {
 		sc.setExpression("p.id = '1'");
 		String sql = sc.toSql();
 		System.out.println(sql);
+	}
+	@Test
+	public void getTableReference(){
+		SimpleCondition sc = new SimpleCondition();
+		sc.setExpression("ert.p.name.b.p.q!=weq.od.qq.pr");
+		List<String> refs = sc.getTableReferences();
+		assertEquals("ert", refs.get(0));
+		assertEquals("weq", refs.get(1));
+	}
+	@Test
+	public void replaceExprWith(){
+		SimpleCondition sc = new SimpleCondition();
+		sc.setExpression("pew.p.name.b.p.q=oop.eeq.qq");
+		sc.replaceExprWith("w");
+		
+//		assertEquals("w.p.name.b.p.q", sc.getExpression());
 	}
 }
