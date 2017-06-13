c8d7aa9411d5aa1415359e4ee733bb82ff9e5661
yuanchi <eeit54@gmail.com>
2017-06-13 11:11:51 +0800

    Expressible.java updated

--- dynasql3/src/main/java/com/jerrylin/dynasql3/Expressible.java
+++ dynasql3/src/main/java/com/jerrylin/dynasql3/Expressible.java
@@ -7,17 +7,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public interface Expressible {
-	static final String REGEX = "(\\w+)\\.[\\w\\.]+";
+	static final String TABLE_REFERENCE = "(\\w+)\\.[\\w\\.\\*]+";
+	static final Pattern FIND_TABLE_REFERENCE = Pattern.compile(TABLE_REFERENCE);
 	public String getExpression();
 	public void setExpression(String expression);
-	default List<String> getTableReferences(){
-		Pattern p = Pattern.compile(REGEX);
-		Matcher m = p.matcher(getExpression());
+	static List<String> findTableReferences(String expression){
+		Matcher m = FIND_TABLE_REFERENCE.matcher(expression);
 		List<String> refs = new ArrayList<>();
 		while(m.find()){
 			String g1 = m.group(1);
 			refs.add(g1);
 		}
+		return refs;
+	
+	}
+	default List<String> getTableReferences(){
+		List<String> refs = findTableReferences(getExpression());
 		return refs;
 	}
 	default boolean withSameTableRef(){
@@ -28,16 +33,8 @@ public interface Expressible {
 		String compared = refs.get(0);
 		return refs.size() == Collections.frequency(refs, compared);
 	}
-	/**
-	 * if all table references are the same, replace all them with new one
-	 * @param tableReference
-	 */
-	default boolean replaceExprWith(String tableReference){
-		if(!withSameTableRef()){
-			return false;
-		}
-		Pattern p = Pattern.compile(REGEX);
-		Matcher m = p.matcher(getExpression());
+	static String getNewExprFrom(String expr, String tableReference){
+		Matcher m = FIND_TABLE_REFERENCE.matcher(expr);
 		// ref. https://stackoverflow.com/questions/38296673/replace-group-1-of-java-regex-with-out-replacing-the-entire-regex
 		StringBuffer newExpr = new StringBuffer();
 		while(m.find()){
@@ -45,7 +42,18 @@ public interface Expressible {
 			m.appendReplacement(newExpr, m.group(0).replaceFirst(Pattern.quote(g1), tableReference));
 		}
 		m.appendTail(newExpr);
-		setExpression(newExpr.toString());
+		return newExpr.toString();
+	}
+	/**
+	 * if all table references are the same, replace all them with new one and update the expression
+	 * @param tableReference
+	 */
+	default boolean replaceExprWith(String tableReference){
+		if(!withSameTableRef()){
+			return false;
+		}
+		String newExpr = getNewExprFrom(getExpression(), tableReference);
+		setExpression(newExpr);
 		return true;
 	}
 }
